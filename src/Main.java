32963da573df3bd4d34daab5edb09436b7d3a388
Anastasiya <makeeva.anastasya89@gmail.com>
2023-02-05 14:54:39 +0300

    formatBMI

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        double Bmi = service.calculate(52, 160);
-        System.out.println(String.format("%(.2f", Bmi));
+        double bmi = service.calculate(52, 160);
+        System.out.println(String.format("%(.2f", bmi));
     }
 }
